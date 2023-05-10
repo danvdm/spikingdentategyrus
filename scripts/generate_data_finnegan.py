c7ab93855768206bc96428327e86080a7f5fc019
dvdmeer <d.v.d.meer473@gmail.com>
2023-05-10 22:56:01 +0200

    added measure for pattern separation

--- scripts/generate_data_finnegan.py
+++ scripts/generate_data_finnegan.py
@@ -9,9 +9,9 @@ from tools.common_parameters import *
 method = "stepwise"             # method for generating the prototypes
 percent_on = 0.1                # percentage of bits that are flipped
 stimulus_length = 200      # length of the pattern
-n_prototypes = 5        # number of prototypes
-n_per_prototype = 10     # max 10!!          # number of variations for subclasses per prototype
-n_per_subclass= 10         # number of variations per subclass
+n_seed_patterns = 5        # number of prototypes
+n_prototype_per_seed = 10     # max 10!!          # number of variations for subclasses per prototype
+n_variations_per_prototype= 10         # number of variations per subclass
 plot_prototype = 4              # index of the prototype to plot
 flip_first_round = 0.1     # percentage of bits that are flipped in the replication
 flip_second_round = 0.05     # percentage of bits that are flipped in the replication
@@ -19,11 +19,11 @@ flip_second_round = 0.05     # percentage of bits that are flipped in the replic
 
 
 print("\nPlease run the data generation file if not done so yet!!")
-print("\nNumber of seed patterns:                     ", n_prototypes)
-print("Number of prototypes per seed patterns       ", n_per_prototype)
-print("Number of prototypes total:                  ", n_per_prototype * n_prototypes)
-print("Number of variations per prototype           ", n_per_subclass) 
-print("Number of patterns total                     ", n_per_prototype * n_prototypes * n_per_subclass) 
+print("\nNumber of seed patterns:                     ", n_seed_patterns)
+print("Number of prototypes per seed patterns       ", n_prototype_per_seed)
+print("Number of prototypes total:                  ", n_prototype_per_seed * n_seed_patterns)
+print("Number of variations per prototype           ", n_variations_per_prototype) 
+print("Number of patterns total                     ", n_prototype_per_seed * n_seed_patterns * n_variations_per_prototype) 
 print("Number of input neurons (stimulus length):   ", stimulus_length)
 print("Number of hidden neurons:                    ", N_hidden)
 print("Number of class neurons:                     ", N_class)
@@ -32,9 +32,9 @@ print("Number of class neurons per class:           ", n_c_unit)
 
 
 # generate data
-prototypes = generate_prototypes(n_prototypes, percent_on, stimulus_length)
-prototype_variations = generate_prototype_variations(prototypes, n_per_prototype, flip_first_round)
-final_variations = generate_final_variations(prototype_variations, n_per_subclass, flip_second_round)
+prototypes = generate_prototypes(n_seed_patterns, percent_on, stimulus_length)
+prototype_variations = generate_prototype_variations(prototypes, n_prototype_per_seed, flip_first_round)
+final_variations = generate_final_variations(prototype_variations, n_variations_per_prototype, flip_second_round)
 
 """ imshow(np.row_stack([prototypes[0], prototypes[0], prototypes[0], prototypes[0], prototypes[0], prototypes[0], prototypes[0], prototypes[0], prototypes[0], prototypes[0],
                      prototypes[1], prototypes[1], prototypes[1], prototypes[1], prototypes[1], prototypes[1], prototypes[1], prototypes[1], prototypes[1], prototypes[1], 
